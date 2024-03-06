b7a51d1f9a2c195fce45cfb7cb484b79a94e3331
Davis <davis.morales@courts.mo.gov>
2024-03-06 15:01:26 -0600

    Projects Section Complete

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,95 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
+import Img1 from "../assets/nba-reference.PNG";
+import Img2 from "../assets/nba-reference.PNG";
+import Img3 from "../assets/nba-reference.PNG";
+
 const Work = () => {
   return (
-    <div className="section" id="work">
-      Work
-    </div>
+    <section className='section' id='work'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row gap-x-10'>
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial='hidden'
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'
+          >
+            <div>
+              <h2 className='h2 leading-tight text-accent'>
+                My Latest <br />
+                Projects
+              </h2>
+              <p className='max-w-sm mb-16'>
+                These are my most recent projects.
+              </p>
+              <button className='btn btn-sm'>View all projects</button>
+            </div>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              <div className='group-hover:bg-black/70  w-full h-full absolute z-40 transition-all duration-300'></div>
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={Img2}
+                alt=''
+              />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>
+                  Utilizing APIs to Display Unique Data in React
+                </span>
+              </div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>NBA Reference</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial='hidden'
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'
+          >
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              <div className='group-hover:bg-black/70  w-full h-full absolute z-40 transition-all duration-300'></div>
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={Img1}
+                alt=''
+              />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>
+                  Utilizing APIs to Display Unique Data in React
+                </span>
+              </div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>NBA Reference</span>
+              </div>
+            </div>
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              <div className='group-hover:bg-black/70  w-full h-full absolute z-40 transition-all duration-300'></div>
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={Img3}
+                alt=''
+              />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>
+                  Utilizing APIs to Display Unique Data in React
+                </span>
+              </div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>NBA Reference</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
